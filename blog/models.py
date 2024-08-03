76a022d6b80fd10bfb2558c92c9cda335a447e5b
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-08-03 12:33:21 +0000

    extended post model

--- blog/models.py
+++ blog/models.py
@@ -13,4 +13,11 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f'The title of this post is "{self.title}" | written by "{self.author}"'
