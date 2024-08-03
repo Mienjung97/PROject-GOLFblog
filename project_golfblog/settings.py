528e9cea02817bfd31d0db16b2b298f1a72ebb7c
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-08-03 12:12:58 +0000

    set secret key

--- project_golfblog/settings.py
+++ project_golfblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-grdw9qmhv++-moo!@rx4f*kog2qk1loxdr810k*_o)+(d&96qy'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
