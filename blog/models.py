4eb544c87d9427bc1ef470cb81253fab5988e532
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-08-03 12:43:35 +0000

    Added generic post view

--- blog/models.py
+++ blog/models.py
@@ -21,3 +21,23 @@ class Post(models.Model):
 
     def __str__(self):
         return f'The title of this post is "{self.title}" | written by "{self.author}"'
+
+class Comment(models.Model):
+    """
+    Stores a single comment entry related to :model:`auth.User`.
+    and :model:`blog.Post`
+    """
+    post = models.ForeignKey(Post, on_delete = models.CASCADE,
+        related_name = "comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f'Comment: "{self.body}" | written by "{self.author}"'
